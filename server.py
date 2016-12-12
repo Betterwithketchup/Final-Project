7ed2fe5eec0f30c24f9ed62aa110bf1e1fadb562
Betterwithketchup <betterwithketchup@gmail.com>
2016-12-12 16:35:02 -0500

    basic game working

--- server.py
+++ server.py
@@ -1,38 +1,56 @@
-from flask import Flask, request, render_template, jsonify, session, redirect,url_for
+from flask import Flask, request, render_template, jsonify, session, redirect,url_for,Response
 from Models import *
 import os
 app = Flask(__name__)
+USERNAME="THIS"
+
 
 @app.route('/',methods=['GET'])
 def nothere():
 	return redirect(url_for('login'))
 
+#login method
 @app.route('/login', methods=['GET','POST'])
 def login():
-	
 	if session.get('logged_in')==True:
+		print(session.get('logged_in'))
 		return redirect(url_for('main'))
 	else:
 		print(session.get('logged_in'))
 		return render_template('login.html',title="Login?",)
 
+#logout method
+@app.route('/weout', methods=['POST'])
+def out():
+	session['id']=None
+	session.pop('logged_in',False)
+	print("logged?:"+str(session.get('logged_in')))
+	return redirect(url_for('login'))
+
+#check against database and load
 @app.route('/data', methods=['POST'])
 def data():
 	name=request.form['username']
 	password=request.form['password']
 	user=User.query.filter_by(username=name).first()
 	if islegit(name,password)=="True":
+		print(user.username)
 		session['id']=user.id
 		session['logged_in']=True
-		print(session.get('id'))
-
+		session['user']=user.username
+		session.modified = True
+		USERNAME=user.id
+		print(USERNAME)
+		#response=Response(str(USERNAME))
+		#response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
 		return redirect(url_for('main'))
 	else:
 		session['logged_in']=False
 		return redirect(url_for('login'))
+
 def islegit(Name,wordpass):
-	print("atleast")
 	user = User.query.filter_by(username=Name).first()
+	#print(user.username)
 	if user != None:
 		if bcrypt.check_password_hash(user.password, wordpass)==True:
 			return "True"
@@ -48,18 +66,39 @@ def addin():
 	Users.inituser(newuser,newpass)
 	return "Done"#redirect(url_for('login'))
 
-@app.route('/main', methods=['POST','GET'])
+@app.route('/main', methods=['GET'])
 def main():
+	print(session.get('logged_in'))
 	if session.get('logged_in')==True:
-		return render_template('mainpage.html',title="Welcome",)
-	return redirect(url_for('login'))
+		print("session:"+str(session.get('user')))
+	#	session['logged_in']=True
+		return render_template('mainpage.html',content=str(session.get('id')),)
+	else:
+		return redirect(url_for('login'))
 
-@app.route('/load', methods=['GET'])
+#AAAAHHHHHHHHH
+@app.route('/load', methods=['GET','POST'])
 def load():
-	user=User.query.filter_by(id=(session.get('id'))).first()
-	thingy = [user.id]
-	return thingy
+	if session.get('logged_in')==True:
+		print("THISTHING")
+	thinger=request.form['name']
+	print(thinger)
+	#name=username
+	print("this is a name: "+str(session.get('user')))
+	#print(USERNAME)
+	#user=User.query.filter_by(username=name).first()
+	# thingy = [user.id,user.username,user.charname]
+	# return jsonify(thinger)
+	# else:
+	# 	return "na"
+	#session['id']
+	#session.pop('logged_in',False)
+	#print("logged?:"+str(session.get('user')))
+	response = Response(thinger)
+	response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+	response.headers['Access-Control-Allow-Credentials'] = 'true'	
+	return response
 
 if __name__ == "__main__":
-	app.secret_key = os.urandom(20)
+	app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'#os.urandom(20)
 	app.run('127.0.0.1',3000,debug=True)
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,18 +1,54 @@
-userid=""
+var $userid=""
+class player{
+	constructor(){
+
+	}
+}
 $(document).ready(function(){
+/*	console.log("Ping!");
 	$('#login').on('click', function(event){
 		//event.preventDefault();
-    	var $username = $("input[name='name']").val();
+    	var $username = $("input[name='username']").val();
     	var $password = $("input[name='password']").val();
+    	console.log($username)
     	$.ajax({
         method: "POST",
         url: "http://127.0.0.1:3000/data",
         data: {'username':$username,'password':$password},
-        //dataType: 'json',
         success:function(response){
+        	$userid=$username;
+        	console.log("HEYO")
+        	window.location.replace("http://stackoverflow.com");
+
 			}
 		})
+	})*/
+	$('#load').on('click', function(){
+		$.ajax({
+			method:"POST",
+			url: "http://127.0.0.1:3000/load",
+			xhrFields: { withCredentials:true },
+			data: {'name':$('.h1').val()},
+			success: function (response) {
+
+		$('#load').attr('class',response)
+
+      	//console.log("done!"+ something.getAllResponseHeaders());
+      	console.log(response)
+      	console.log($('.title').html)
+    }
+		});
+
 	})
+	/*$('#logout').on('click', function(event){
+    	$.ajax({
+        method: "POST",
+        url: "http://127.0.0.1:3000/weout",
+        success:function(response){
+        	//console.log(response)
+			}
+		})
+	})*/
 	$('#register').on('click', function(){
 		$('#form').empty()
 		$('#form').append(`
@@ -34,30 +70,32 @@ $(document).ready(function(){
 		})
 	})
 
-
-
 })
+
+
+
+//the actual game
 var Game = {
     display: null,
     map: {},
+    engine: null,
+    player: null,
     
     init: function() {
         this.display = new ROT.Display();
         document.body.appendChild(this.display.getContainer());
-        this.display.drawText(5,  2, "Hello world");
-        $.ajax({
-		        method: "GET",
-		        url: "http://127.0.0.1:3000/load",
-			
-        success: function(response){
-
-        }
-        })
+        
         this._generateMap();
+        
+        var scheduler = new ROT.Scheduler.Simple();
+        scheduler.add(this.player, true);
+
+        this.engine = new ROT.Engine(scheduler);
+        this.engine.start();
     },
     
     _generateMap: function() {
-        var digger = new ROT.Map.Digger();
+        var digger = new ROT.Map.Arena(80,30);
         var freeCells = [];
         
         var digCallback = function(x, y, value) {
@@ -69,9 +107,18 @@ var Game = {
         }
         digger.create(digCallback.bind(this));
         
-        //this._generateBoxes(freeCells);
-        
+        this._generateBoxes(freeCells);
         this._drawWholeMap();
+        this._createPlayer(freeCells);
+    },
+    
+    _createPlayer: function(freeCells) {
+        var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+        var key = freeCells.splice(index, 1)[0];
+        var parts = key.split(",");
+        var x = parseInt(parts[0]);
+        var y = parseInt(parts[1]);
+        this.player = new Player(x, y);
     },
     
     _generateBoxes: function(freeCells) {
@@ -92,3 +139,49 @@ var Game = {
     }
 };
 
+var Player = function(x, y) {
+    this._x = x;
+    this._y = y;
+    this._draw();
+}
+    
+Player.prototype.act = function() {
+    Game.engine.lock();
+    window.addEventListener("keydown", this);
+}
+    
+Player.prototype.handleEvent = function(e) {
+    var keyMap = {};
+    keyMap[87] = 0;
+    keyMap[33] = 1;
+    keyMap[68] = 2;
+    keyMap[34] = 3;
+    keyMap[83] = 4;
+    keyMap[35] = 5;
+    keyMap[65] = 6;
+    keyMap[36] = 7;
+
+    var code = e.keyCode;
+    /* one of numpad directions? */
+    if (!(code in keyMap)) { return; }
+
+    /* is there a free space? */
+    var dir = ROT.DIRS[8][keyMap[code]];
+    var newX = this._x + dir[0];
+    var newY = this._y + dir[1];
+    var newKey = newX + "," + newY;
+    if (!(newKey in Game.map)) { return; }
+
+    Game.display.draw(this._x, this._y, Game.map[this._x+","+this._y]);
+    this._x = newX;
+    this._y = newY;
+    this._draw();
+    window.removeEventListener("keydown", this);
+    Game.engine.unlock();
+}
+
+Player.prototype._draw = function() {
+    Game.display.draw(this._x, this._y, "@", "#ff0");
+}    
+
+Game.init();
